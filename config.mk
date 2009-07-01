e164913af482f15fa3e4122392231cc2d64ef356
Ramil Farkhshatov <noble_yoda@mail.ru>
2009-07-01 19:18:17 +0400

    Not deleting *.o files while building new ones.
    This allows to fix bindings and reload them in REPL.

--- config.mk
+++ config.mk
@@ -5,7 +5,6 @@ GSC = ${shell [ `which gambit-gsc 2>/dev/null` ] && echo gambit-gsc || echo gsc}
 all: ${targets}
 
 %.o1: %.scm
-	-rm -f $@ 2>/dev/null
 	${GSC} ${GSCFLAGS} -cc-options "${CFLAGS}" -ld-options "${LDFLAGS}" $<
 
 clean:
